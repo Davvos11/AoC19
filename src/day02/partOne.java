e94475b0218c2f06f6bb2fa4bf9d6daed2c3d41a
Davvos11 <vosdavid2@gmail.com>
2019-12-02 22:22:20 +0100

    Day02 part one cleanUp

--- src/day02/partOne.java
+++ src/day02/partOne.java
@@ -1,14 +1,9 @@
 package day02;
 
-import static java.lang.System.exit;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.URL;
 
 public class partOne {
