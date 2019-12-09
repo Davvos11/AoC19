916d2c728e40bff3d429c96d7d8a59cbd4bc28a1
Davvos11 <vosdavid2@gmail.com>
2019-12-09 20:58:28 +0100

    Day03 part one manhattan distance

--- src/day03/calculateDistance.java
+++ src/day03/calculateDistance.java
@@ -3,6 +3,7 @@ package day03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class calculateDistance {
@@ -23,6 +24,18 @@ public class calculateDistance {
 
     public static void main(String[] args) {
         calculateDistance p = new calculateDistance();
-        System.out.println(p.setUp());
+        List<List<Integer>> intersects = p.setUp();
+
+        intersects.remove(0);
+
+        int closest = -1;
+        for (List<Integer> i : intersects){
+            int distance = Math.abs(i.get(0)) + Math.abs(i.get(1));
+            if (distance < closest || closest == -1){
+                closest = distance;
+            }
+        }
+
+        System.out.println(closest);
     }
 }
