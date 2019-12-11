ee40b9a14792b4a5e115f36afb44a1c0d59a7b3e
Davvos11 <vosdavid2@gmail.com>
2019-12-11 22:57:23 +0100

    Day04 part one

---
+++ src/day04/partOne.java
@@ -0,0 +1,41 @@
+package day04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class partOne {
+    public static void main(String[] args) {
+        String input = "273025-767253";
+        int low  = Integer.parseInt(input.split("-")[0]);
+        int high = Integer.parseInt(input.split("-")[1]);
+
+        List<Integer> possiblePasswords = new ArrayList<>();
+
+        for (int i = low; i <= high; i++) {
+            // Add digits to array:
+            List<Integer> digits= new ArrayList<>();
+            for (int d = 5; d >= 0; d--) {
+                int digit = (int) (( i % Math.pow(10,d+1) ) / Math.pow(10,d));
+                digits.add(digit);
+            }
+
+            // Check decreasing and adjacent
+            boolean decreasing = false;
+            boolean adjacent = false;
+            for (int j = 0; j < digits.size()-1; j++) {
+                if (digits.get(j) > digits.get(j + 1)) {
+                    decreasing = true;
+                }
+                if (digits.get(j).equals(digits.get(j + 1))){
+                    adjacent = true;
+                }
+            }
+
+            if (!decreasing && adjacent){
+                possiblePasswords.add(i);
+            }
+        }
+        System.out.println(possiblePasswords);
+        System.out.println(possiblePasswords.size());
+    }
+}
